3126f821b44bc2660bfa7aa0a3b84343a5e222fa
harshit031196 <guptaharshit0311@gmail.com>
2020-10-29 00:47:54 +0530

    Added the connection between the java program and database

---
+++ src/main/java/com/employeepayrolljdbc/DBDemo.java
@@ -0,0 +1,40 @@
+package com.employeepayrolljdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo {
+	
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+		String user = "root";
+		String password = "harshit";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded successfully!");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Driver Missing!", e);
+		}
+		
+		listDrivers();
+		
+		try {
+			connection = DriverManager.getConnection(jdbcURL, user, password);
+			System.out.println("Connection successfully established!" + connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while(driverList.hasMoreElements()) {
+			Driver driver = driverList.nextElement();
+			System.out.println("   "+driver.getClass().getName());
+		}
+		
+	}
+}
