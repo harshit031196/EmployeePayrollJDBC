c18bd1e2aa5b9608289630254e5d5a576d6712c2
harshit031196 <guptaharshit0311@gmail.com>
2020-11-03 06:45:17 +0530

    Added the functionality to add new employee in the json server and check the count of the new employee using threads

--- src/test/java/com/employeepayrolljdbc/EmployeePayrollJsonServerRestAssuredTest.java
+++ src/test/java/com/employeepayrolljdbc/EmployeePayrollJsonServerRestAssuredTest.java
@@ -1,9 +1,14 @@
 package com.employeepayrolljdbc;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -39,6 +44,42 @@ public class EmployeePayrollJsonServerRestAssuredTest {
 		request.body(jsonString);
 		return request.post("/employees"); 
 	}
+	private Map<Integer, Boolean> addMultipleEmployeesToJsonServer(List<EmployeePayrollData> employeePayrollDataList) {
+		Map<Integer, Boolean> employeeInsertionStatus = new HashMap<Integer, Boolean>();
+		Map<Integer, Boolean> employeeAdditionStatusCodes = new HashMap<Integer, Boolean>();
+		employeePayrollDataList.forEach(employeePayrollData -> {
+			employeeInsertionStatus.put(employeePayrollData.hashCode(), false);
+			Runnable task = () -> {
+				System.out.println("Employee Being added: " + Thread.currentThread().getName());
+				
+				Response response = addEmployeeToJsonServer(employeePayrollData);
+				int statusCode = response.getStatusCode();
+				if (statusCode == 201) {
+					employeeInsertionStatus.put(employeePayrollData.hashCode(), true);
+					String responseAsString = response.asString();
+					JsonObject jsonObject = new Gson().fromJson(responseAsString, JsonObject.class);
+					int id = jsonObject.get("id").getAsInt();
+					employeePayrollData.setId(id);
+					employeeAdditionStatusCodes.put(employeePayrollData.hashCode(), true);
+				}
+				else {
+					employeeAdditionStatusCodes.put(employeePayrollData.hashCode(), false);
+				}
+				System.out.println("Employee added: " + Thread.currentThread().getName());
+			};
+			Thread thread = new Thread(task, employeePayrollData.getName());
+			thread.start();
+		});
+		
+		while (employeeInsertionStatus.containsValue(false)) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return employeeAdditionStatusCodes;
+	}
 	
 	@Test
 	public void givenNewEmployee_OnPost_ShouldMatch201ResponseAndCount() {
@@ -57,5 +98,29 @@ public class EmployeePayrollJsonServerRestAssuredTest {
 		employeePayrollService.addEmployeeToPayroll(employeePayrollData, IOService.REST_IO);
 		long enteries = employeePayrollService.countEntries(IOService.REST_IO);
 		assertEquals(4, enteries);
+	}
+	
+	@Test
+	public void givenMultipleEmployee_OnPost_ShouldMatch201ResponseAndCount() {
+		EmployeePayrollData[] arrayOfEmps = getEmployeeList();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		List<EmployeePayrollData> employeePayrollDataList = new ArrayList<EmployeePayrollData>() {{
+			add(new EmployeePayrollData(0, "Daksh", "Santa Cruz, Mumbai", 60000,
+				LocalDate.now(), "M".charAt(0), "Capgemini", "Sales"));
+			add(new EmployeePayrollData(0, "Arvind", "Hauz Khas, New Delhi", 50000,
+					LocalDate.now(), "M".charAt(0), "Capgemini", "Production"));
+			add(new EmployeePayrollData(0, "Vaishali", "Whitefield, Bangalore", 45000,
+					LocalDate.now(), "F".charAt(0), "Capgemini", "Finance"));
+		}};
+		
+		Map<Integer, Boolean> employeeAdditionStatusCodes = addMultipleEmployeesToJsonServer(employeePayrollDataList);
+		assertFalse(employeeAdditionStatusCodes.containsValue(false));
+		employeePayrollDataList.forEach(employeePayrollData -> {
+			if(employeeAdditionStatusCodes.get(employeePayrollData.hashCode())) {
+				employeePayrollService.addEmployeeToPayroll(employeePayrollData, IOService.REST_IO);
+			}
+		});
+		long enteries = employeePayrollService.countEntries(IOService.REST_IO);
+		assertEquals(7, enteries);
 	}
 }
